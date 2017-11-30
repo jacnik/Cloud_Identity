fd99ff4f487a5546d23cf645ef301bb03c7ec374
Jacnik <you@example.com>
2017-11-30 20:55:57 +0000

    client app files actually commited

---
+++ ClientApplication/Startup.cs
@@ -0,0 +1,62 @@
+﻿namespace ClientApplication
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddAuthentication(options =>
+            //    {
+            //        options.DefaultScheme = "cookie";
+            //        options.DefaultChallengeScheme = "oidc";
+            //    })
+            //    .AddCookie("cookie")
+            //    .AddOpenIdConnect("oidc", options =>
+            //    {
+            //        options.Authority = "http://localhost:8211/";
+            //        options.ClientId = "openIdConnectClient";
+            //        options.SignInScheme = "cookie";
+            //    }
+            //);
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
