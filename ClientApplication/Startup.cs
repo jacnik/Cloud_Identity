f4bc40493cd1dac711608640574520005bb9538b
Jacnik <you@example.com>
2017-12-04 18:16:28 +0000

    Redirect to IdentityServer works but throws error

--- ClientApplication/Startup.cs
+++ ClientApplication/Startup.cs
@@ -17,19 +17,19 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddAuthentication(options =>
-            //    {
-            //        options.DefaultScheme = "cookie";
-            //        options.DefaultChallengeScheme = "oidc";
-            //    })
-            //    .AddCookie("cookie")
-            //    .AddOpenIdConnect("oidc", options =>
-            //    {
-            //        options.Authority = "http://localhost:8211/";
-            //        options.ClientId = "openIdConnectClient";
-            //        options.SignInScheme = "cookie";
-            //    }
-            //);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("cookie")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "http://localhost:8211/";
+                options.ClientId = "openIdConnectClient";
+                options.SignInScheme = "cookie";
+                options.RequireHttpsMetadata = false; // todo for development ONLY, use HTTPS in production
+            });
 
             services.AddMvc();
         }
@@ -37,20 +37,21 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseBrowserLink();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
 
             app.UseStaticFiles();
-
-            //app.UseAuthentication();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
